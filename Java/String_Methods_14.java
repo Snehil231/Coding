ce9ab979f7b17632a4ca4793f1f30b277552946c
Snehil Harsh <snehilharsh2000@gmail.com>
2025-12-18 08:12:03 +0530

    Updated String_Methods_14.java

--- Java/String_Methods_14.java
+++ Java/String_Methods_14.java
@@ -1,11 +1,81 @@
-import java.util.Scanner;
 public class String_Methods_14 
 {
     public static void main(String[] args) 
     {
-     Scanner sc = new Scanner(System.in);
+      /*1  
+       String name = "Snehil";
+       int index  = name.length();
+       System.out.println(index);
+     */
 
-        
+       /*2  
+       String name1 = "SNEHIL";
+       String lowercase = name1.toLowerCase();
+       System.out.println(lowercase);
+       */ 
+
+       /* 3 
+       String name2 = "snehil";
+       String Uppercase = name2.toUpperCase();
+       System.out.println(Uppercase);
+       */
+
+       /*  4
+       String name3 = "     Snehil    ";
+       String trim  = name3.trim();
+       System.out.println(trim);
+       */
+
+       /*  5
+       String name4 = "Rajeshwar";
+       String intstart = name4.substring(3);
+       System.out.println(intstart);
+         */
+
+        /*  6
+       String name5 = "Rajeshwar";
+       String intstartend = name5.substring(3, 8);
+       System.out.println(intstartend);
+         */
+
+       /*  7
+         String a = "Snehil";
+         boolean truefalse =a.startsWith("Sn");
+         System.out.println(truefalse);
+
+          String a1 = "Snehil";
+         boolean truefalse1 =a.startsWith("Rt");
+         System.out.println(truefalse1);
+         */
+
+         /* 8 
+         String b = "Happy";
+         String replace = b.replace('p', 'r');
+         System.out.println(replace);
+
+         String b1 = "Rainy";
+         String replace1 = b1.replace("iny", "joz");
+         System.out.println(replace1);
+
+         String b2 = "Sunny"; 
+         String replace2 = b2.replace("n","abc");
+         System.out.println(replace2);
+         */
+
+         /*  
+         String  name = "Snehil";
+         Boolean startswith = name.startsWith("Sn");
+         System.out.println(startswith);
+         Boolean endwith = name.endsWith("il");
+         System.out.println(endwith);
+
+         Boolean startswith1 = name.startsWith("sd");
+         System.out.println(startswith1);
+         Boolean endwith1 = name. endsWith("gs");
+         System.out.println(endwith1);
+         */
+
+         
     }
     
 }
